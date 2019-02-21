82b0440959f365d24536f47e7f288917aa7f0c24
jrkinouani <kinouani@gmail.com>
2019-02-21 21:15:08 +0400

    add new code pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,15 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def contact
+
+  end
+
+  def projets
+
+  end
+  def service
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
-  root  "pages#home"
-  resources :projets 
+  get "/home" => "pages#home"
+  get "/service" => "pages#service"
+  get "/projets" => "pages#projets"
+  get "/contact" => "pages#contact"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
