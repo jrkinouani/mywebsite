6b173763d8740b05b1c09012647dec3a5471f57b
jrkinouani <kinouani@gmail.com>
2019-03-27 13:36:33 +0400

    add sendmail with tuto

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,16 +1,5 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.action_mailer.delivery_method = :smtp
-   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-   ActionMailer::Base.smtp_settings = {
-     :user_name => ENV['SENDGRID_USERNAME'],
-     :password => ENV['SENDGRID_PASSWORD'],
-     :domain => 'heroku.com',
-     :address => 'smtp.sendgrid.net',
-     :port => 587,
-     :authentication => :plain,
-     :enable_starttls_auto => true
-   }
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
@@ -62,4 +51,15 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+   config.action_mailer.delivery_method = :smtp
+   ActionMailer::Base.smtp_settings = {
+     :user_name => ENV['SENDGRID_USERNAME'],
+     :password => ENV['SENDGRID_PASSWORD'],
+     :domain => 'heroku.com',
+     :address => 'smtp.sendgrid.net',
+     :port => '587',
+     :authentication => :plain,
+     :enable_starttls_auto => true
+   }
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,16 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.action_mailer.delivery_method = :smtp
-   config.action_mailer.default_url_options = { :host => 'jrkinouani.herokuapp.com', :protocol => 'https'}
-   ActionMailer::Base.smtp_settings = {
-     :user_name => ENV['SENDGRID_USERNAME'],
-     :password => ENV['SENDGRID_PASSWORD'],
-     :domain => 'heroku.com',
-     :address => 'smtp.sendgrid.net',
-     :port => 587,
-     :authentication => :plain,
-     :enable_starttls_auto => true
-   }
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -99,5 +89,16 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+   config.action_mailer.default_url_options = { host: 'https://jrkinouani.herokuapp.com'}
+   config.action_mailer.delivery_method = :smtp
 
+   ActionMailer::Base.smtp_settings = {
+     :user_name => ENV['SENDGRID_USERNAME'],
+     :password => ENV['SENDGRID_PASSWORD'],
+     :domain => 'heroku.com',
+     :address => 'smtp.sendgrid.net',
+     :port => '587',
+     :authentication => :plain,
+     :enable_starttls_auto => true
+   }
 end
